74b0a80874bfe8c2969a0bfae95f4897046ec112
Nikithagudipati <nikithagudipati@gmail.com>
2026-01-09 20:25:21 +0530

    unwanted files

--- backend/src/middleware1/role.middleware.js
+++
@@ -1,8 +0,0 @@
-module.exports = (...allowedRoles) => {
-  return (req, res, next) => {
-    if (!allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ msg: 'Access denied' });
-    }
-    next();
-  };
-};
